13e5fed99081703fdf3657971c161f69017db046
Dahrius93 <dariolauretta93@gmail.com>
2026-05-18 09:02:24 +0200

    Logo

--- components/navbar/Logo.tsx
+++ components/navbar/Logo.tsx
@@ -1,5 +1,21 @@
+import Link from "next/link";
+import { Button } from "../ui/button";
+import { VscCode } from "react-icons/vsc";
+
 function Logo() {
-  return <div>Logo</div>;
+  return (
+    // asChild è una proprietà di ShadCn che fa in modo di passare
+    // tutte le proprietà del pulsante al figlio (in questo caso un link)
+    // in sostanza un link con la forma di un pulsante.
+
+    // altrimenti, senza asChild, sarebbe un pulsante con all'interno un link
+    // e non sarebbe valido per l'HTML
+    <Button size="icon" asChild>
+      <Link href="/">
+        <VscCode className="w-6 h-6" />
+      </Link>
+    </Button>
+  );
 }
 
 export default Logo;
