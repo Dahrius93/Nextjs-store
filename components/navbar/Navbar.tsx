080a315373cc7bb512aff561a9c78c0887c9c94c
Dahrius93 <dariolauretta93@gmail.com>
2026-05-18 15:55:46 +0200

    Search

--- components/navbar/Navbar.tsx
+++ components/navbar/Navbar.tsx
@@ -4,12 +4,19 @@ import DarkMode from "./DarkMode";
 import CartButton from "./CartButton";
 import NavSearch from "./NavSearch";
 import Container from "../global/Container";
+import { Suspense } from "react";
+
 function Navbar() {
   return (
     <nav className="border-b ">
       <Container className="flex flex-col sm:flex-row  sm:justify-between sm:items-center flex-wrap gap-4 py-8">
         <Logo />
-        <NavSearch />
+        {/* Suspense è richiesto da Next.js perché NavSearch usa useSearchParams(),
+            un hook che legge l'URL lato client e non è disponibile durante il SSR */}
+        <Suspense>
+          <NavSearch />
+        </Suspense>
+
         <div className="flex gap-4 items-center ">
           <CartButton />
           <DarkMode />
