0a7a05980859a94675e5720d32c026523d2bdb43
Dahrius93 <dariolauretta93@gmail.com>
2026-05-18 11:28:28 +0200

    fix prisma V7 (from v5-v6)

--- app/about/page.tsx
+++ app/about/page.tsx
@@ -1,7 +1,24 @@
-import React from "react";
+import db from "@/utils/db";
 
-function AboutPage() {
-  return <div>AboutPage</div>;
+async function AboutPage() {
+  const profile = await db.testProfile.create({
+    data: {
+      name: "random name",
+    },
+  });
+
+  const users = await db.testProfile.findMany();
+
+  return (
+    <div>
+      {users.map((user) => {
+        return (
+          <h2 key={user.id} className="text-2xl font-bold">
+            {user.name}
+          </h2>
+        );
+      })}
+    </div>
+  );
 }
-
 export default AboutPage;
