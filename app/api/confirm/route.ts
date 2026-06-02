cb0c72fb8e1ca4b9df343a8b72de773f10e50c47
Dahrius93 <dariolauretta93@gmail.com>
2026-06-02 16:20:16 +0200

    back to original code for confirm route for cart not deleted after payment done

--- app/api/confirm/route.ts
+++ app/api/confirm/route.ts
@@ -9,35 +9,24 @@ export const GET = async (req: NextRequest) => {
   const { searchParams } = new URL(req.url);
   const session_id = searchParams.get("session_id") as string;
 
-  // evito session_id = null dato che è as string
-  if (!session_id) return Response.json(null, { status: 400 });
-
   try {
     const session = await stripe.checkout.sessions.retrieve(session_id);
     const orderId = session.metadata?.orderId;
     const cartId = session.metadata?.cartId;
-
     if (session.status === "complete") {
-      // se pagamento effettuato controllo se isPaid su db è ancora false
-      // e se si lo imposto a true e cancello il carrello
-      // se operazione già eseguita evito di cancellare un carrello che non esiste
-      // e quindi ricevere errore 500 da server
-      const order = await db.order.findUnique({ where: { id: orderId } });
-      if (!order?.isPaid) {
-        await db.order.update({
-          where: {
-            id: orderId,
-          },
-          data: {
-            isPaid: true,
-          },
-        });
-        await db.cart.delete({
-          where: {
-            id: cartId,
-          },
-        });
-      }
+      await db.order.update({
+        where: {
+          id: orderId,
+        },
+        data: {
+          isPaid: true,
+        },
+      });
+      await db.cart.delete({
+        where: {
+          id: cartId,
+        },
+      });
     }
   } catch (err) {
     console.log(err);
